ccecfe3f47e01bebc5214a88715aaca7782b757f
BalkoYana <balko.yana1@student.uzhnu.edu.ua>
2023-12-12 19:33:46 +0200

    changes

---
+++ resources/views/factory/show.blade.php
@@ -0,0 +1,6 @@
+<a href="{{route('factory.index')}}">C</a><br/>
+<h1>{{ $factory->name }}</h1>
+<p>Code: {{ $factory->code }}</p>
+<p>Number: {{ $factory->number }}</p>
+<p>Branch: {{ $factory->branch }}</p>
+<p>Address: {{ $factory->address }}</p>
